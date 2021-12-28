69955ac32f5812045fb508820eebc5b4d35cce23
Assaf Gadish <agadish@gmail.com>
2021-12-28 14:16:08 +0200

    taking 1024 points for ex1b

--- part1.m
+++ part1.m
@@ -27,7 +27,7 @@ N = 1024;
 %w = linspace(0, pi, N);
 s_xx_a_mone = poly(s_xx_a_zeros);
 s_xx_a_mehane = [1];
-s_xx_a = freqz(s_xx_a_mone, s_xx_a_mehane);
+s_xx_a = freqz(s_xx_a_mone, s_xx_a_mehane, N);
 w = linspace(0, pi, length(s_xx_a));
 
 figure;
@@ -38,7 +38,7 @@ ylabel('|S_{xx_a}(e^{j\omega})|');
 
 s_xx_b_mone = [1];
 s_xx_b_mehane = poly(s_xx_b_poles);
-s_xx_b = freqz(s_xx_b_mone, s_xx_b_mehane);
+s_xx_b = freqz(s_xx_b_mone, s_xx_b_mehane, N);
 w = linspace(0, pi, length(s_xx_b));
 
 figure;
@@ -46,7 +46,6 @@ plot(w, abs(s_xx_b));
 title('Spectrum of the AR(5) process, x_{b}[n]')
 xlabel('\omega [rad]');
 ylabel('|S_{xx_b}(e^{j\omega})|');
-
 
 
 function [all_roots] = add_conj_inverse_roots(roots)

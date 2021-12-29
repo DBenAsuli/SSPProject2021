b76c0c87567bf11b5675c1b1527776aa08f3c937
Dvir Ben Asuli <dvir971@gmail.com>
2021-12-29 23:03:59 +0200

    Question 4 Started

--- part1.m
+++ part1.m
@@ -8,13 +8,96 @@ x_a_mone = [1 -0.4 -0.69 -0.964 -0.714];
 x_b_mehane = [1 0.6 0.34 -0.034 -0.03807 -0.4212];
 
 %%% Question 1
-[Sxx_a, Sxx_b] = question1(x_a_mone, x_b_mehane);
+%[Sxx_a, Sxx_b] = question1(x_a_mone, x_b_mehane);
 
 %%% Question 2
 % question2(x_a_mone, x_b_mehane);
 
 %%% Question 3
-question3(x_a_mone, x_b_mehane, Sxx_a, Sxx_b);
+%question3(x_a_mone, x_b_mehane, Sxx_a, Sxx_b);
+
+%%% Question 4
+question4(x_a_mone, x_b_mehane);
+
+
+
+function [] = question4(x_a_mone, x_b_mehane)
+    question4a(x_a_mone, x_b_mehane, 1000)
+end
+
+function [] = question4a(x_a_mone, x_b_mehane, M)
+    K = 1024;
+    N = 400;
+
+    sxxa_array = zeros(M,K);
+    sxxb_array = zeros(M,K);
+
+    sxxa = calc_real_spectrum(x_a_mone, [1], K);
+    sxxb = calc_real_spectrum([1], x_b_mehane, K);
+
+    for m = (1:M)
+        [xa, xb] = generate_xa_xb(x_a_mone, x_b_mehane);
+        Xa_k = get_positive_fft(xa, K);
+        Sxxa_pos_periodogram = 1 / N * (Xa_k .* conj(Xa_k));
+        sxxa_array(m,:) = Sxxa_pos_periodogram;
+
+        Xb_k = get_positive_fft(xb, K);
+        Sxxb_pos_periodogram = 1 / N * (Xb_k .* conj(Xb_k));
+        sxxb_array(m,:) = Sxxb_pos_periodogram;
+    end
+
+    [sxxa_hat, Ba, std_a, RMSE_a] = calc_empiric_values(sxxa_array, sxxa);
+    [sxxb_hat, Bb, std_b, RMSE_b] = calc_empiric_values(sxxb_array, sxxb);
+
+    plot_empiric_values(sxxa, Ba, std_a, RMSE_a, 'Periodogram for x_a[n]');
+    plot_empiric_values(sxxb, Bb, std_b, RMSE_b, 'Periodogram for x_b[n]');
+
+end
+
+function [sxx_hat, B, std, RMSE] = calc_empiric_values(sxx_array, sxx)
+    sxx_hat = empiric_average(sxx_array);
+    B = empiric_bias(sxx_hat, sxx);
+    V = empiric_variance(sxx_hat, sxx_array);
+    std = V.^0.5;
+    MSE = empiric_mse(sxx_array, sxx);
+    RMSE = MSE.^0.5;
+end
+
+function [] = plot_empiric_values(sxx, B, std, RMSE, varname)
+    figure;
+    title(strcat('Performance:', {' '}, varname, '[n]'))
+    hold on
+    plot_positive_spectrum_graph_only(sxx);
+    plot_positive_spectrum_graph_only(B);
+    plot_positive_spectrum_graph_only(std);
+    plot_positive_spectrum_graph_only(RMSE);
+    hold off
+    legend('sxx', 'Bias', 'std', 'RMSE')
+end
+
+function [sxx_hat] = empiric_average(sxx_array)
+    M = size(sxx_array, 1);
+    sxx_hat = sum(sxx_array)';
+    sxx_hat = sxx_hat / M;
+end
+
+function [B] = empiric_bias(sxx_hat, sxx)
+    B = sxx_hat - sxx;
+end
+
+function [V] = empiric_variance(sxx_hat, sxx_array)
+    M = size(sxx_array, 1);
+    v_array = (sxx_array - sxx_hat').^2;
+    V = sum(v_array)';
+    V = V/ M;
+end
+
+function [MSE] = empiric_mse(sxx_array, sxx)
+    M = size(sxx_array, 1);
+    v_array = (sxx_array - sxx').^2;
+    V = sum(v_array)';
+    MSE = V/ M;
+end
 
 function [] = question3(x_a_mone, x_b_mehane, Sxx_a, Sxx_b)
     % Section a: biased correlogram estimator
@@ -87,7 +170,7 @@ end
 function ffted_x_positive = get_positive_fft(x, K)
     x_padded = [x' zeros(1, 2 * K - size(x, 1))];
     ffted_x = fftshift(fft(x_padded));
-    ffted_x_positive = ffted_x(K + 1 : 2 * K);
+    ffted_x_positive = ffted_x(K + 1 : 2 * K)';
 end
 
 function Rxx = estimate_biased_correlogram(x, N)
@@ -124,6 +207,16 @@ function Rxx_l = estimate_unbiased_correlogram_l(x, l, N)
         Rxx_l = Rxx_l + x(n) * x(n + abs_l);
     end
     Rxx_l = Rxx_l / (N - abs_l);
+end
+
+function sxx = calc_real_spectrum(x_mone, x_mehane, N)
+    x_mone_roots = roots(x_mone);
+    x_mehane_roots = roots(x_mehane);
+    sxx_zeros = add_conj_inverse_roots(x_mone_roots);
+    sxx_poles = add_conj_inverse_roots(x_mehane_roots);
+    sxx_mone = poly(sxx_zeros);
+    sxx_mehane = poly(sxx_poles);
+    sxx = freqz(sxx_mone, sxx_mehane, N);
 end
 
 function [s_xx_a, s_xx_b] = question1(x_a_mone, x_b_mehane)
